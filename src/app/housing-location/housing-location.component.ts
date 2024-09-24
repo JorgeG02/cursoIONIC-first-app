d2f0e9f084750b8b0e0db62f04cea02d936b5358
JorgeG02 <124932762+JorgeG02@users.noreply.github.com>
2024-09-24 11:56:01 +0200

    Agregado componente housing-location

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styles: ``,
+  styleUrls: ['./housing-location.component.css'],
+})
+export class HousingLocationComponent {
+
+}
